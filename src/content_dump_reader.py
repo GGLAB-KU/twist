388d499a48b42919e2c737128f85c35ad7f605af
gsoykan <gsoykan@sabanciuniv.edu>
2024-03-11 23:40:28 +0300

    analyze scientific -vs non scientific

--- src/content_dump_reader.py
+++ src/content_dump_reader.py
@@ -10,6 +10,7 @@ import pandas as pd
 import wikipediaapi
 from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize, word_tokenize
+from openai import OpenAI
 from torchmetrics import MeanMetric
 from torchmetrics.text import TranslationEditRate
 from tqdm import tqdm
@@ -58,10 +59,14 @@ class ContentDumpReader:
 
     def _read_dump_as_df_dict(self,
                               dump_alias: str,
-                              convert_to_dict_with_id_1: bool = False) -> List[Dict] | Dict[str, List[Dict]]:
+                              convert_to_dict_with_id_1: bool = False,
+                              keep_na_mt_target: bool = False) -> List[Dict] | Dict[str, List[Dict]]:
         csv_path = self.csv_dict[dump_alias]
         content_df = pd.read_csv(csv_path, dtype={'id_2': pd.StringDtype()})
-        content_df = content_df.dropna(subset=['mt', 'target'])
+
+        if not keep_na_mt_target:
+            content_df = content_df.dropna(subset=['mt', 'target'])
+
         df_records = content_df.to_dict('records')
         df_records.sort(key=lambda x: len(x['target']), reverse=True)
 
@@ -99,12 +104,15 @@ class ContentDumpReader:
         df_records = self._read_dump_as_df_dict(dump_alias)
         df_records = list(filter(lambda x: 5 < len(x['target']) <= 250, df_records))
 
+        return self.compute_ter_from_records(df_records)
+
+    def compute_ter_from_records(self, records) -> float:
         ter = TranslationEditRate().to('cuda')
         current_batch_preds = []
         current_batch_targets = []
         batch_size = 128
 
-        for i, row in enumerate(tqdm(df_records)):
+        for i, row in enumerate(tqdm(records)):
             pred = row['mt']
             target = row['target']
             current_batch_preds.append(pred)
@@ -122,9 +130,12 @@ class ContentDumpReader:
         if filter_by_len:
             df_records = list(filter(lambda x: 5 < len(x['target']) <= 250, df_records))
 
+        return self.compute_ned_from_records(df_records)
+
+    def compute_ned_from_records(self, records) -> float:
         metric = MeanMetric()
 
-        for i, row in enumerate(tqdm(df_records)):
+        for i, row in enumerate(tqdm(records)):
             pred = row['mt']
             target = row['target']
             edit_distance = Levenshtein.distance(pred, target)
@@ -141,12 +152,15 @@ class ContentDumpReader:
         if filter_by_len:
             df_records = list(filter(lambda x: 5 < len(x['target']) <= 250, df_records))
 
+        return self.compute_edit_operations_by_type_from_records(df_records)
+
+    def compute_edit_operations_by_type_from_records(self, records):
         delete_metric = MeanMetric()
         equal_metric = MeanMetric()
         insert_metric = MeanMetric()
         replace_metric = MeanMetric()
 
-        for i, row in enumerate(tqdm(df_records)):
+        for i, row in enumerate(tqdm(records)):
             pred = row['mt']
             target = row['target']
             opcodes = Levenshtein.opcodes(pred, target)
@@ -181,21 +195,26 @@ class ContentDumpReader:
         if filter_by_len:
             df_records = list(filter(lambda x: 5 < len(x['target']) <= 250, df_records))
 
+        return self.compute_mt_eq_target_from_records(df_records)
+
+    def compute_mt_eq_target_from_records(self, records):
         count = 0
-        for i, row in enumerate(tqdm(df_records)):
+        for i, row in enumerate(tqdm(records)):
             pred = row['mt']
             target = row['target']
             if pred == target:
                 count += 1
 
-        return count, len(df_records)
+        return count, len(records)
 
     def compare_sentence_word_len(self,
                                   dump_alias: str):
         all_df_records = self._read_dump_as_df_dict(dump_alias)
 
         # all_df_records = list(filter(lambda x: 200 < len(x['target']) <= 250, all_df_records))
+        return self.compare_sentence_word_len_from_records(all_df_records)
 
+    def compare_sentence_word_len_from_records(self, records):
         def count(df_records):
             pred_sent_lens = []
             target_sent_lens = []
@@ -239,7 +258,7 @@ class ContentDumpReader:
                         np.array(counts[2])[ind].mean(),
                         np.array(counts[3])[ind].mean())
 
-        all_records_counts = count(all_df_records)
+        all_records_counts = count(records)
         pred_sent_lens, target_sent_lens, _, target_token_lens = all_records_counts
 
         all_records_stats = compute_stats(all_records_counts, target_word_len_range=None)
@@ -299,6 +318,46 @@ class ContentDumpReader:
         else:
             raise ValueError(f'Multiple Dump sections found => {dump_section}')
 
+    def _read_raw_json(self, dump_alias: str) -> Dict:
+        dump_folder_path = os.path.join(self.data_dir, dump_alias)
+        files = os.listdir(dump_folder_path)
+        files = list(filter(lambda f: f.endswith('.json'), files))
+        text_json_path = None
+        for file in files:
+            file_path = os.path.join(dump_folder_path, file)
+            if 'text' in file_path:
+                text_json_path = file_path
+
+        csv_items = {}
+
+        with open(text_json_path, 'r') as file:
+            array_items = ijson.items(file, 'item')
+            for item in tqdm(array_items, desc='processing json'):
+                item_id = item['id']
+
+                item_source, item_mt, item_target = (item.get('source') or {}).get('content'), (
+                        item.get('mt') or {}).get(
+                    'content'), (item.get('target') or {}).get('content')
+
+                item_mt_engine = (
+                        item.get('mt') or {}).get(
+                    'engine')
+
+                item_id_first, item_id_second = item_id.split('/')
+                csv_item = [
+                    item_id,
+                    item_id_first,
+                    item_id_second,
+                    item_source if item_source != '' else None,
+                    item_mt if item_mt != '' else None,
+                    item_mt_engine,
+                    item_target if item_target != '' else None,
+                ]
+
+                csv_items[item_id] = csv_item
+
+        return csv_items
+
     def _dump_to_csv(self, dump_alias: str) -> str:
         dump_folder_path = os.path.join(self.data_dir, dump_alias)
         files = os.listdir(dump_folder_path)
@@ -328,6 +387,11 @@ class ContentDumpReader:
         # ijson.common.IncompleteJSONError: parse error: unallowed token at this point in JSON text
         #           p></p>"         }     },     ,     {         "id": "431229/c
         #                      (right here) ------^
+
+        #      "id": "64344/mwAQ",
+        #          ...
+        #         "mt": {
+        #             "engine": "Yandex",
 
         html_dict = {}
         with open(html_json_path, 'r') as file:
@@ -474,6 +538,229 @@ class ContentDumpReader:
                 elif user_input == '':
                     break  # Breaks the inner while loop and continues with the next iteration of the for loop
 
+    ### analysis on scientific vs non-scientific
+    def scientific_analysis_count_without_mts(self,
+                                              translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        translation_pairs_df = pd.read_csv(translation_pair_path)
+
+        dump_alias = reader.content_dumps[1]
+        df_records = reader._read_dump_as_df_dict(dump_alias,
+                                                  convert_to_dict_with_id_1=True)
+
+        translation_pairs = translation_pairs_df.to_dict('records')
+
+        total_num_documents = 0
+        total_num_sections = 0
+        num_docs_without_mts = 0
+        num_sections_without_mts = 0  # en az 1 section'ı mt si olanlar arasında
+        num_not_founds = 0
+
+        for translation_pair in tqdm(translation_pairs, desc='iterating...'):
+            if translation_pair['scientific?'] not in ['TRUE', 'FALSE']:
+                continue
+
+            total_num_documents += 1
+
+            doc_id = translation_pair['Unnamed: 0']
+            result = reader.get_dump_document_with_id_records_by_doc_id_1(df_records,
+                                                                          doc_id)
+
+            # id,id_1,id_2,source,mt,target,source_html_hyperlinks,target_htm_hyperlinks
+            if len(result) == 0:
+                num_not_founds += 1
+            else:
+                total_num_sections += len(result)
+                current_no_mts = 0
+                for record in result:
+                    if pd.isna(record['mt']):
+                        num_sections_without_mts += 1
+                        current_no_mts += 1
+
+                if current_no_mts == len(result):
+                    num_docs_without_mts += 1
+
+        return {
+            'total_num_documents': total_num_documents,
+            'total_num_sections': total_num_sections,
+            'num_docs_without_mts': num_docs_without_mts,
+            'num_sections_without_mts': num_sections_without_mts,
+            'num_not_founds': num_not_founds
+        }
+
+    def _scientific_convert_to_records(self,
+                                       translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        translation_pairs_df = pd.read_csv(translation_pair_path)
+        dump_alias = reader.content_dumps[1]
+        df_records = reader._read_dump_as_df_dict(dump_alias,
+                                                  convert_to_dict_with_id_1=True)
+        translation_pairs = translation_pairs_df.to_dict('records')
+
+        valid_records = []
+        for translation_pair in tqdm(translation_pairs, desc='iterating...'):
+            if translation_pair['scientific?'] not in ['TRUE', 'FALSE']:
+                continue
+
+            doc_id = translation_pair['Unnamed: 0']
+            result = reader.get_dump_document_with_id_records_by_doc_id_1(df_records,
+                                                                          doc_id)
+
+            # id,id_1,id_2,source,mt,target,source_html_hyperlinks,target_htm_hyperlinks
+            if len(result) == 0:
+                continue
+            else:
+                for record in result:
+                    valid_records.append({'mt': record['mt'], 'target': record['target']})
+
+        valid_records.sort(key=lambda x: len(x['target']), reverse=True)
+        return valid_records
+
+    def scientific_compute_ter(self,
+                               translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        valid_records = self._scientific_convert_to_records(translation_pair_path)
+
+        # getting the rid of the instance that has outlier target len
+        valid_records = list(filter(lambda x: len(x['target']) <= 250, valid_records))
+        return self.compute_ter_from_records(valid_records)
+
+    def scientific_compute_ned(self,
+                               translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        valid_records = self._scientific_convert_to_records(translation_pair_path)
+
+        return self.compute_ned_from_records(valid_records)
+
+    def scientific_compute_edit_ops(self,
+                                    translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        valid_records = self._scientific_convert_to_records(translation_pair_path)
+
+        return self.compute_edit_operations_by_type_from_records(valid_records)
+
+    def scientific_compute_mt_eq_target(self,
+                                        translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        valid_records = self._scientific_convert_to_records(translation_pair_path)
+
+        return self.compute_mt_eq_target_from_records(valid_records)
+
+    def scientific_compare_sentence_word_len(self,
+                                             translation_pair_path: str = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/wikimedia-mt-analysis - translation-pairs_final.csv'):
+        valid_records = self._scientific_convert_to_records(translation_pair_path)
+
+        return self.compare_sentence_word_len_from_records(valid_records)
+
+    def fill_mt_engine_in_samples(self):
+        file_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/scientific_non-sci_samples.csv'
+        updated_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/updated_scientific_non-sci_samples.csv'
+        df = pd.read_csv(file_path)
+
+        dump_alias = reader.content_dumps[1]
+
+        mt_engine_key = 'mt-engine\n(google, yandex etc..)'
+
+        raw_json = self._read_raw_json(dump_alias)
+
+        def update_mt_engine(row):
+            doc_id = row['id']
+
+            raw_json_item = raw_json.get(doc_id)
+            mt_engine = raw_json_item[5]
+
+            return mt_engine
+
+        df[mt_engine_key] = df.apply(update_mt_engine, axis=1)
+
+        df.to_csv(updated_path, index=False)
+
+    def fill_chatgpt_in_samples(self):
+        file_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/scientific_non-sci_samples.csv'
+        updated_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/updated_scientific_non-sci_samples.csv'
+        df = pd.read_csv(file_path)
+
+        # enter your api key
+        client = OpenAI(
+            api_key="",
+        )
+
+        def translate_with_chatgpt(row):
+            source_text = row['source']
+
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=[
+                    {"role": "system",
+                     "content": "You are an expert wikipedian translating articles from English to Turkish."},
+                    {"role": "user", "content": f"translate the following from English to Turkish: \"{source_text}\""}
+                ]
+            )
+
+            chatgpt_text = completion.choices[0].message.content
+
+            return chatgpt_text
+
+        df['chatgpt'] = df.apply(translate_with_chatgpt, axis=1)
+
+        df.to_csv(updated_path, index=False)
+
+    def extract_translation_qualities(self,
+                                      ):
+        file_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/scientific_non-sci_chatgpt.csv'
+        updated_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/exported_scientific_non-sci_chatgpt.csv'
+        df = pd.read_csv(file_path)
+
+        df_records = df.to_dict('records')
+
+        instances = []
+
+        for record in df_records:
+            mt_target_type = record['Type'].lower()
+            chatgpt_result = record['chatgpt - result']
+            assert 'bad mt' in mt_target_type or 'good mt' in mt_target_type, 'wrong mt val'
+            assert 'bad post' in mt_target_type or 'good post' in mt_target_type, 'wrong target val'
+            assert 'good' in chatgpt_result or 'bad' in chatgpt_result
+            mt_result = True if 'good mt' in mt_target_type else False
+            target_result = True if 'good post' in mt_target_type else False
+            chatgpt_result = True if 'good' in chatgpt_result else False
+            id = record['id']
+            is_scientific = True if record['doc type '].lower() == 'scientific' else False
+
+            latin_greek_term = record['latin term\nchanged /\nunchanged']
+
+            if pd.isna(latin_greek_term):
+                mt_changed = latin_greek_term
+            else:
+                mt_changed = 'mt-changed' in latin_greek_term
+
+            mt_engine = record['mt-engine\n(google, yandex etc..)']
+
+            instance = [id,
+                        mt_result,
+                        target_result,
+                        chatgpt_result,
+                        is_scientific,
+                        mt_changed,
+                        mt_engine]
+            instances.append(instance)
+
+        df = pd.DataFrame(instances,
+                          columns=['id',
+                                   'mt',
+                                   'target',
+                                   'chatgpt',
+                                   'is_scientific',
+                                   'mt_latin_changed',
+                                   'mt_engine'])
+
+        df.to_csv(updated_path, index=False)
+
+    def analyze_translation_quailities(self):
+        updated_path = '/home/gsoykan/Desktop/ku/wikimedia-mt-analysis/data/exported_scientific_non-sci_chatgpt.csv'
+        data = pd.read_csv(updated_path)
+        quality_distribution = data[['mt', 'target', 'chatgpt']].apply(pd.Series.value_counts)
+        quality_percentage = quality_distribution / len(data) * 100
+        consistent_quality = data[(data['mt'] == data['target']) & (data['target'] == data['chatgpt'])]
+
+        consistent_quality_summary = consistent_quality[['mt', 'target', 'chatgpt']].apply(pd.Series.value_counts)
+
+        return quality_distribution, quality_percentage, consistent_quality_summary, quality_rates, quality_by_type
+
 
 if __name__ == '__main__':
     reader = ContentDumpReader(num_dumps=2)
@@ -508,6 +795,31 @@ if __name__ == '__main__':
     #
     # reader.update_translation_pairs_for_not_found()
 
-    reader.end_to_end_annotation_mode()
+    # reader.end_to_end_annotation_mode()
 
-# [f"{item['id']}\t{item['id_1']}\t{item['id_2']}\t\t{item['source']}\t{item['mt']}\t{item['target']}" for item in result]
+    #### scientific values
+
+    # scientific_counts = reader.scientific_analysis_count_without_mts()
+    # print(scientific_counts)
+
+    # ter_value = reader.scientific_compute_ter()
+    # print(ter_value)
+
+    # ned_value = reader.scientific_compute_ned()
+    # print(ned_value)
+    # mean_ops_by_type = reader.scientific_compute_edit_ops()
+    # print(mean_ops_by_type)
+
+    # mt_eq_target = reader.scientific_compute_mt_eq_target()
+    # print(mt_eq_target)
+
+    # stats = reader.scientific_compare_sentence_word_len()
+    # print(stats)
+
+    # reader.fill_mt_engine_in_samples()
+
+    # reader.fill_chatgpt_in_samples()
+
+    # reader.extract_translation_qualities()
+
+    reader.analyze_translation_quailities()
